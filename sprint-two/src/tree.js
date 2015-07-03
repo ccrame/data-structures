3b8cec3eed20b87792ef128c4c0087baa7e1c81d
Charles Crame <ccrame@gmail.com>
2015-07-03 12:43:11 -0700

    Completed tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,8 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree,treeMethods);
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -15,11 +16,29 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  this.children.push(Tree(value));
 
 };
 
 treeMethods.contains = function(target){
-
+// we have a tree, and a target we're looking for
+// we start at the top node (main tree)
+// start by checking value of top node against target
+  var result = false;
+  if (this.value === target) {
+    return true;
+  } else {
+    var i = 0;
+    while (!result && i < this.children.length) {
+      result = this.children[i].contains(target);
+      i++;
+    }
+  }
+  return result;
+// if matched, return true, we're done
+// if not, check through children
+// for each child, we do the steps from line 3 on
+//if nothing found in 
 };
 
 
